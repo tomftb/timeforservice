ef94d8a01415e9ea9fb9ad92bcdb02b96c63f961
Tomasz Borczynski <tborczynski87@gmail.com>
2024-08-31 01:27:53 +0200

    extend homepage function with twig template with variables

--- src/Controller/MainConotroller.php
+++ src/Controller/MainConotroller.php
@@ -2,15 +2,26 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainConotroller
+class MainConotroller extends AbstractController
 {
     #[Route('/', name: 'app_home', methods: ['GET'])]
     public function homepage(): Response
     {
         // dd($this);
-        return new Response('<p>homepage</p>', 200);
+        $starshipCount = 457;
+        
+        $test = [
+            'sss'=>'aaa',
+            'bb'=>'yyy',
+        ];
+        
+        return $this->render('main/homepage.html.twig',[
+            'starshipCount'=>$starshipCount,
+            'test'=>$test
+        ]);
     }
 }
