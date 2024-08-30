c17f73ebe5924ab8d9339997ff4d3029549f4486
Tomasz Borczynski <tborczynski87@gmail.com>
2024-08-30 18:46:46 +0200

    extend method homepage with response

--- src/Controller/MainConotroller.php
+++ src/Controller/MainConotroller.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
-
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class MainConotroller
 {
-
+    #[Route('/',name: "app_home",methods:['GET'])]
+    public function homepage():Response
+    {
+        //dd($this);
+        return new Response('<p>homepage</p>',200);
+    }
 }
