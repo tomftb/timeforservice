8962cc5186bb27993c1730746a49c2626e1e5c9b
Tomasz Borczynski <tborczynski87@gmail.com>
2024-08-30 21:34:13 +0200

    cs-fixer

--- src/Controller/MainConotroller.php
+++ src/Controller/MainConotroller.php
@@ -2,15 +2,15 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
 class MainConotroller
 {
-    #[Route('/',name: "app_home",methods:['GET'])]
-    public function homepage():Response
+    #[Route('/', name: 'app_home', methods: ['GET'])]
+    public function homepage(): Response
     {
-        //dd($this);
-        return new Response('<p>homepage</p>',200);
+        // dd($this);
+        return new Response('<p>homepage</p>', 200);
     }
 }
