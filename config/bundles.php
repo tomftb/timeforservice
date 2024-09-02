5ea65f82377b80fca4e279c3180e7b0633670061
Tomasz Borczynski <tborczynski87@gmail.com>
2024-09-02 13:03:18 +0200

    install doctrine with docker ext

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
