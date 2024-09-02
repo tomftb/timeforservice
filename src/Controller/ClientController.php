55bc0e9acbc26f88e89617386ba7e89ca74cb548
Tomasz Borczynski <tborczynski87@gmail.com>
2024-09-02 22:21:38 +0200

    add new method new

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\ClientRepository;
+use App\Entity\Client;
+use Doctrine\ORM\EntityManagerInterface;
 /**
  * Description of ClientController
  *
@@ -21,10 +23,31 @@ class ClientController extends AbstractController{
         if(!$client){
             throw $this->createNotFoundException('Client not found');   
         }
-        
-        
         return $this->render('client/show.html.twig',[
             'client'=>$client
         ]);
+    }
+    #[Route('/new', name: 'app_client_new', methods: ['GET'])]
+    public function new(EntityManagerInterface $entityManager): Response
+    {
+        
+        $client = new Client();
+        $client->setName('Farmline Sp. z o.o.');
+        $client->setStreet('Paderewskiego 3');
+        $client->setZipCode('87-162');
+        $client->setTown('Lubicz Górny');
+        $client->setNin('879-264-70-97');
+        $client->setStatus('NEW');
+        
+        $entityManager->persist($client);
+        $entityManager->flush();
+        //echo "<pre>";
+        //var_dump($client);
+        //echo "</pre>";
+        //dd('new clinet');
+        return new Response(sprintf(
+                "Client created - %d",
+                $client->getId()
+        ));
     }
 }
