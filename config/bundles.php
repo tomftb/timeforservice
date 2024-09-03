68a03e3b6a1513e977b2e5431b2579b641d57223
Tomasz Borczynski <tborczynski87@gmail.com>
2024-09-03 13:28:34 +0200

    install knp-time-bundle, pagerfanta-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,6 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
+    BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
 ];
