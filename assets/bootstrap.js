bd04e2d50bc93908e96bc6bd23843be8f07143e5
Tomasz Borczynski <tborczynski87@gmail.com>
2024-09-06 11:12:46 +0200

    add comment

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,3 +1,4 @@
+/* The @ symbol isn't important: that's just a character namespaced JavaScript packages user. */
 import { startStimulusApp } from '@symfony/stimulus-bundle';
 
 const app = startStimulusApp();
