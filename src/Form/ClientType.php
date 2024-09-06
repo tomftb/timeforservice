5409f93fa22a3a495285cf12297a8ec4bd06bc0c
Tomasz Borczynski <tborczynski87@gmail.com>
2024-09-06 13:35:30 +0200

    add new validations

--- src/Form/ClientType.php
+++ src/Form/ClientType.php
@@ -6,17 +6,34 @@ use App\Entity\Client;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Validator\Constraints\Length;
 
 class ClientType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('street')
-            ->add('zipCode')
-            ->add('town')
-            ->add('nin')
+            ->add('name',TextType::class,[
+                'required' => true,
+                'constraints' => [new Length(['min' => 3])],
+            ])
+            ->add('street',TextType::class,[
+                'required' => true,
+                'constraints' => [new Length(['min' => 3])],
+            ])
+            ->add('zipCode',TextType::class,[
+                'required' => true,
+                'constraints' => [new Length(['min' => 5])],
+            ])
+            ->add('town',TextType::class,[
+                'required' => true,
+                'constraints' => [new Length(['min' => 3])],
+            ])
+            ->add('nin',TextType::class,[
+                'required' => true,
+                'constraints' => [new Length(['min' => 10])],
+            ])
         ;
     }
 
