88c57c600f3898397fa3642e7f939481e6901b96
Tomasz Borczynski <tborczynski87@gmail.com>
2024-09-06 13:05:55 +0200

    remove blank rows

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -31,13 +31,9 @@ class ClientController extends AbstractController{
     #[Route('/new', name: 'app_client_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
-       
         $client = new Client();
-         
         $form = $this->createForm(ClientType::class, $client);
-        
         $form->handleRequest($request);
-        
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->persist($client);
             $entityManager->flush();
