78d1b81a97e780cee1539a1919411b5995a785ac
Tomasz Borczynski <tborczynski87@gmail.com>
2024-09-07 23:14:21 +0200

    register popover

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,6 +1,6 @@
 /* The @ symbol isn't important: that's just a character namespaced JavaScript packages user. */
 import { startStimulusApp } from '@symfony/stimulus-bundle';
+import Popover from 'stimulus-popover'
 
 const app = startStimulusApp();
-// register any custom, 3rd party controllers here
-// app.register('some_controller_name', SomeImportedController);
+app.register('popover', Popover);
