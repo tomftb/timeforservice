4f6dcc69a58471f8b1732a7b0c477fa337e4595b
Tomasz Borczynski <tborczynski87@gmail.com>
2024-09-08 00:35:55 +0200

    extend original popover with remeber data feature

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,6 +1,4 @@
 /* The @ symbol isn't important: that's just a character namespaced JavaScript packages user. */
 import { startStimulusApp } from '@symfony/stimulus-bundle';
-import Popover from 'stimulus-popover'
 
 const app = startStimulusApp();
-app.register('popover', Popover);
