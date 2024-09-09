11d0f0f7843cb46a4439bd6ce7f81db2970df2ef
Tomasz Borczynski <tborczynski87@gmail.com>
2024-09-09 16:38:17 +0200

    add sort option

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -23,20 +23,26 @@ class MainController extends AbstractController
         ClientPointRepository $clientPointRepository,
         /* VARIABLE NAME MUST EQUAL URL PART ex. ?page => $page */
         #[MapQueryParameter] int $page = 1,
+        #[MapQueryParameter] string $sort = 'endedAt',
+        #[MapQueryParameter] string $sortDirection = 'ASC',
         #[MapQueryParameter] string $query = null,
         #[MapQueryParameter('clientsPoints', \FILTER_VALIDATE_INT)] array $searchClientsPoints = [],
     ): Response
     {
+        $validSorts = ['description','clientPoint','user','endedAt'];
+        $sort = in_array($sort,$validSorts) ? $sort : 'endedAt';
         $maxPerPage = 2;
         $pager = Pagerfanta::createForCurrentPageWithMaxPerPage(
-                new QueryAdapter($serviceRepository->findBySearchWithClientPointQueryBuilder($query, $searchClientsPoints)),
-                $page,
+                new QueryAdapter($serviceRepository->findBySearchWithClientPointQueryBuilder($query, $searchClientsPoints,$sort,$sortDirection),false),
+               $page,
                 $maxPerPage
         );
         return $this->render('main/homepage.html.twig', [
             'services' => $pager,
             'clients' => $clientRepository->findAll(),
             'searchClientsPoints' => $searchClientsPoints,
+            'sort' => $sort,
+            'sortDirection' => $sortDirection
         ]);
     }
 }
