69fc02339e78383ede202f8f76a4b8c5aa8467b7
Tomasz Borczynski <tborczynski87@gmail.com>
2024-09-10 09:26:13 +0200

    enable full page transitions

--- assets/app.js
+++ assets/app.js
@@ -8,5 +8,23 @@ import './bootstrap.js';
 import './styles/app.css';
 /* SIMILAR TO Webpack Encore - in Encore there is no need to add extension .js*/
 import testScript from './lib/test-script.js';
+import { shouldPerformTransition, performTransition } from "turbo-view-transitions";
 testScript('In peace?',false);
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+
+
+document.addEventListener("turbo:before-render", (event) => {
+  if (shouldPerformTransition()) {
+    event.preventDefault();
+
+    performTransition(document.body, event.detail.newBody, async () => {
+      await event.detail.resume();
+    });
+  }
+});
+
+document.addEventListener("turbo:load", () => {
+  // View Transitions don't play nicely with Turbo cache
+  if (shouldPerformTransition()) Turbo.cache.exemptPageFromCache();
+});
