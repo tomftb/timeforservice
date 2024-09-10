02279aee837fcc072d47640010dfc9b7af30890f
Tomasz Borczynski <tborczynski87@gmail.com>
2024-09-10 09:37:02 +0200

    add new method to run transition with turbo-frame, and add exceptions for ex. popover

--- assets/app.js
+++ assets/app.js
@@ -12,10 +12,10 @@ import { shouldPerformTransition, performTransition } from "turbo-view-transitio
 testScript('In peace?',false);
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
 
-
+let skipNextRenderTransition = false;
 
 document.addEventListener("turbo:before-render", (event) => {
-  if (shouldPerformTransition()) {
+  if (shouldPerformTransition() && !skipNextRenderTransition) {
     event.preventDefault();
 
     performTransition(document.body, event.detail.newBody, async () => {
@@ -27,4 +27,17 @@ document.addEventListener("turbo:before-render", (event) => {
 document.addEventListener("turbo:load", () => {
   // View Transitions don't play nicely with Turbo cache
   if (shouldPerformTransition()) Turbo.cache.exemptPageFromCache();
+});
+
+document.addEventListener("turbo:before-frame-render", (event) => {
+  if (shouldPerformTransition() && !event.target.hasAttribute('data-skip-transition')) {
+    event.preventDefault();
+    skipNextRenderTransition = true;
+    setTimeout(()=>{
+        skipNextRenderTransition = false;
+    },100);
+    performTransition(event.target, event.detail.newFrame, async () => {
+      await event.detail.resume();
+    });
+  }
 });
