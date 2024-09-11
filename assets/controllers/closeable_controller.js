f61521ccd25a5fba8e697e8f744f2601be1603a3
Tomasz Borczynski <tborczynski87@gmail.com>
2024-09-11 11:06:07 +0200

    new version

--- assets/controllers/closeable_controller.js
+++ assets/controllers/closeable_controller.js
@@ -1,20 +1,8 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-    async close(){
-        this.element.style.width='0';
-        
-        await this.#waitForAnimation();
-        
-        this.element.remove();
-        
-    }
-    /*
-     * # - set methoad as private
-     */
-    #waitForAnimation(){
-        return Promise.all(
-            this.element.getAnimations().map((animation) => animation.finished),
-        );
-    }
+   close() {
+       console.log(this);
+       this.element.remove();
+   }
 }
