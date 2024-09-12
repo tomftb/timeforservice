3cb8301601f133d141d868944b6f0a49aee162fe
Tomasz Borczynski <tborczynski87@gmail.com>
2024-09-12 15:42:46 +0200

    add fade in effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,24 @@ module.exports = {
   content: [
     "./vendor/tales-from-a-dev/flowbite-bundle/templates/**/*.html.twig",
     "./assets/**/*.js",
-    "./templates/**/*.html.twig",
+    "./templates/**/*.html.twig"
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [
+    extend: {
+        animation: {
+            'fade-in' : 'fadeIn .5s ease-out;'
+        },
+        keyframes: {
+            fadeIn: {
+                '0%': { opacity: 0 },
+                '100%': { opacity: 1 }
+            }
+        }
+    }
+    },
+    plugins: [
       plugin(function({addVariant}){
           addVariant('turbo-frame','turbo-frame[src] &')
-      }),
-  ],
+      })
+  ]
 }
