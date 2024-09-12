9a42a1bdb2bb553954a6466957f250b658270a07
Tomasz Borczynski <tborczynski87@gmail.com>
2024-09-12 15:14:58 +0200

    add basic modal operations

---
+++ assets/controllers/modal_controller.js
@@ -0,0 +1,23 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller{
+    static targets = ['dialog'];
+    
+    open(){
+        /*
+         * showModal() adds some extra features
+         */
+        this.dialogTarget.showModal();
+        //this.dialogTarget.show();
+        /*
+         * TURN OFF PAGE SCROOL
+         */
+        document.body.classList.add('overflow-hidden');
+    }
+    close(){
+        if(this.hasDialobTarget){
+            this.dialogTarget.close();
+        }
+        document.body.classList.remove('overflow-hidden');
+    }
+}
