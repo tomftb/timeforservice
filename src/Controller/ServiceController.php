5ccb50f85e9905389cf61d0ad4841e46136cee79
Tomasz Borczynski <tborczynski87@gmail.com>
2024-09-13 22:44:45 +0200

    fix open form modal by ading action with url

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\FormInterface;
 
 #[Route('/service')]
 class ServiceController extends AbstractController
@@ -27,8 +28,9 @@ class ServiceController extends AbstractController
     #[Route('/new', name: 'app_service_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
+        sleep(2);
         $service = new Service();
-        $form = $this->createForm(ServiceType::class, $service);
+        $form = $this->createServiceForm($service);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->persist($service);
@@ -51,7 +53,7 @@ class ServiceController extends AbstractController
     #[Route('/{id}/edit', name: 'app_service_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Service $service, EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm(ServiceType::class, $service);
+        $form = $this->createServiceForm($service);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
@@ -72,5 +74,12 @@ class ServiceController extends AbstractController
             $this->addFlash('success', 'Service deleted!');
         }
         return $this->redirectToRoute('app_service_index', [], Response::HTTP_SEE_OTHER);
+    }
+    private function createServiceForm(Service $service=null ): FormInterface
+    {
+        $service=$service ?? new Service();
+        return $this->createForm(ServiceType::class,$service ,[
+            'action' => $service->getId() ? $this->generateUrl('app_service_edit',['id'=>$service->getId()]) : $this->generateUrl( 'app_service_new' ), 
+        ]);
     }
 }
