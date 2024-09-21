675ba686094be890721dd94e3d237857b94a0409
Tomasz Borczynski <tborczynski87@gmail.com>
2024-09-21 21:42:30 +0200

    set as live component

--- src/Twig/Components/ServiceForm.php
+++ src/Twig/Components/ServiceForm.php
@@ -2,15 +2,38 @@
 namespace App\Twig\Components;
 
 use Symfony\Component\Form\FormView;
-use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+use Symfony\UX\LiveComponent\ComponentWithFormTrait;
+use Symfony\Component\Form\FormInterface;
+use App\Entity\Service;
+use App\Form\ServiceType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+
 
 /**
  * Description of ServiceForm
  *
  * @author Tomasz
  */
-#[AsTwigComponent]
-class ServiceForm {
-    public FormView $form;
+#[AsLiveComponent]
+class ServiceForm extends AbstractController{
     
+    use DefaultActionTrait;
+    use ComponentWithFormTrait;
+
+    #[LiveProp]
+    public ?Service $initialFormData = null;
+    
+    #[\Override]
+    protected function instantiateForm(): FormInterface
+    {
+        //dd($this->initialFormData);
+        $service=$this->initialFormData ?? new Service();
+        
+        return $this->createForm(ServiceType::class,$service ,[
+            'action' => $service->getId() ? $this->generateUrl('app_service_edit',['id'=>$service->getId()]) : $this->generateUrl( 'app_service_new' ), 
+        ]);
+    }
 }
