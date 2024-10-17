146d4a39477e8e42aaa02232973e04d24f9c4ae3
Tomasz Borczynski <tborczynski87@gmail.com>
2024-10-17 22:53:28 +0200

    rename set() to setId(), add setStartedAt() and setEndedAt()

--- assets/controllers/option_controller.js
+++ assets/controllers/option_controller.js
@@ -3,6 +3,9 @@ import { Controller } from '@hotwired/stimulus';
 export default class extends Controller {
     
     idList = new Array();
+    startedAt='';
+    endedAt='';
+    
     static values = {
         url: String
     };
@@ -10,7 +13,7 @@ export default class extends Controller {
         
     connect() {
     }
-    set(t){
+    setId(t){
         this.id = t.srcElement.value;
         var idx = this.idList.indexOf(t.srcElement.value);
         if(idx<0){
@@ -19,12 +22,48 @@ export default class extends Controller {
         else{
             this.idList.splice(idx, 1);
         }
+        console.log('setId',this.id);
+        this.updateUrl();
+    }
+    setStartedAt(t){
+        console.log("setStartedAt",t.srcElement.value);
+        this.startedAt=t.srcElement.value;
+        this.updateUrl();
+    }
+    setEndedAt(t){
+        console.log("setEndedAt",t.srcElement.value);
+        this.endedAt=t.srcElement.value;
         this.updateUrl();
     }
     updateUrl(){
+        let and='?';
+        let url=this.urlValue;
+        /*
+         * SET CLEAR
+         */
+        this.hrefTarget.href = url
+        /*
+         * UPDATE WITH ARRAY LIST
+         */
         if(this.idList.length!==0){
             let tmpList = this.idList.join(',');
-            this.hrefTarget.href = this.urlValue+"?id="+tmpList;
+            this.hrefTarget.href = url+"?id="+tmpList;
+            url='';
+            and='&';
         }
+        /*
+         * UPDATE WITH STARTED AT
+         */
+        if(this.startedAt!==''){
+            this.hrefTarget.href+=and+"startedat="+this.startedAt;
+            and='&';
+        }
+        /*
+         * UPDATE WITH ENDED AT
+         */
+        if(this.endedAt!==''){
+            this.hrefTarget.href+=and+"endedat="+this.endedAt;
+        }
+        console.log("url - ",this.hrefTarget.href);
     }
 }
