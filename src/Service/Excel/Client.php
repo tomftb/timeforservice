400cfd0ba22f5931921bc657dd037b97a10ae9ac
Tomasz Borczynski <tborczynski87@gmail.com>
2024-10-26 15:09:43 +0200

    add sum time

--- src/Service/Excel/Client.php
+++ src/Service/Excel/Client.php
@@ -43,20 +43,62 @@ class Client extends _Main
             'D'=>"Czas(h):"
         ]);
         /*
+         * SET DATA SET SUM COLUMN
+         */
+        parent::setDataSumColumnLabel('E',"Suma(h):");
+        /*
+         * SET DATA SET SUM COLUMN
+         */
+        parent::setDataSumColumn('F');
+        /*
+         * SET DATA SET COLUMN TO SUM
+         */
+        parent::setDataColumnToSum('D');
+        /*
          * SET ROWS WITH DATA
          */
-        self::setData($serviceRepository);     
+        self::setData($serviceRepository);
     }
     private function setData(array $serviceRepository=[]):void
     {
         $lp=1;
+        $timeInH = 0;
         foreach($serviceRepository as $value){
+            $timeInH = parent::getTimeInH(intval($value->getTime(),10));
+            /*
+             * SET FIRST DATA SET ROW
+             */
+            if($this->firstDataSetRow===null){
+                $this->firstDataSetRow = $this->row;
+            }
             $this->activeWorksheet->setCellValue('A'.$this->row, $lp++);
             $this->activeWorksheet->setCellValue('B'.$this->row, $value->getEndedAt()->format('Y-m-d'));
             $this->spreadsheet->getActiveSheet()->getCell('C'.$this->row)->setValue($value->getClientPoint()->getName()."\n".$value->getClientPoint()->getStreet().", ".$value->getClientPoint()->getTown());
             $this->spreadsheet->getActiveSheet()->getStyle('C'.$this->row)->getAlignment()->setWrapText(true);
-            $this->activeWorksheet->setCellValue('D'.$this->row, $value->getTime());
+            $this->activeWorksheet->setCellValue('D'.$this->row, $timeInH);
+            /*
+             * SUM TIME
+             */
+            $this->sumTime+=$timeInH;
+            /*
+             * SET LAST DATA SET ROW
+             */
+            $this->lastDataSetRow =  $this->row;
+            /*
+             * UPDATE ROW
+             */
             $this->row++;
         }
+        /*
+         * SUM
+         */
+        parent::sumDataSetRow();
+        /*
+         * SET DEFAULTS
+         */
+        $this->firstDataSetRow = null;
+        $this->lastDataSetRow = null;
+        $this->sumTime = null;
     }
+
 }
--- src/Service/Excel/_Main.php
+++ src/Service/Excel/_Main.php
@@ -4,7 +4,7 @@ namespace App\Service\Excel;
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
-
+use PhpOffice\PhpSpreadsheet\Calculation\MathTrig\Sum as Sum;
 /**
  * Description of _Main
  *
@@ -14,6 +14,14 @@ abstract class _Main {
     
     protected Spreadsheet $spreadsheet;
     protected $activeWorksheet;
+    protected ?int $dataSetSumRow = null;
+    protected ?int $firstDataSetRow = null;
+    protected ?int $lastDataSetRow = null;
+    private ?string $dataSetSumColumn = null;
+    private ?string $dataSetColumnToSum = null;
+    private ?string $dataSetSumColumnLabel = null;
+    private ?string $dataSetSumColumnLabelValue = null;
+    protected ?float $sumTime = null;
     
     public function __construct(
         private string $appTmp,
@@ -39,13 +47,13 @@ abstract class _Main {
     {
         $this->activeWorksheet->setCellValue('A'.$this->row, $title);
         $this->activeWorksheet->MergeCells($columnsMerge[0].strval($this->row).':'.$columnsMerge[1].strval($this->row));
+        $this->dataSetSumRow=$this->row;
         $this->row++;
     }
     protected function setDescription(array $descriptions=[]):void
     {
         foreach($descriptions as $column => $description){
             $this->activeWorksheet->setCellValue($column.strval($this->row), $description);
-            
         }
         $this->row++;
     }
@@ -57,5 +65,100 @@ abstract class _Main {
         $fileContent = file_get_contents($fileName);
         unlink($fileName);
         return $fileContent;
+    }
+    protected function setDataSumColumn(string $column='A')
+    {
+        $this->dataSetSumColumn = $column;
+    }
+    protected function setDataSumColumnLabel(string $column='A',string $label='')
+    {
+        $this->dataSetSumColumnLabel = $column;
+        $this->dataSetSumColumnLabelValue = $label;
+    }
+    protected function setDataColumnToSum(string $column='A')
+    {
+        $this->dataSetColumnToSum = $column;
+    }
+    protected function sumDataSetRow():void
+    {
+        /*
+         * CHECK IS THERE SOMETHING TO SUM
+         */
+        if($this->firstDataSetRow === null || $this->sumTime===null){
+            return ;
+        }
+        self::checkDataSetRowProperties();
+        /*
+         * SET SUM LABEL
+         */
+        $this->activeWorksheet->setCellValue($this->dataSetSumColumnLabel.$this->dataSetSumRow,$this->dataSetSumColumnLabelValue);
+        /*
+         * SET SUM VALUE
+         */
+        $this->activeWorksheet->setCellValue($this->dataSetSumColumn.$this->dataSetSumRow,$this->sumTime);
+    }
+    private function checkDataSetRowProperties()
+    {
+        /*
+         * CHECK SUM COLUMN
+         */
+        if($this->dataSetSumColumn === null){
+            Throw New \Exception("SET SUM COLUMN");
+        }
+        /*
+         * CHECK COLUMN TO SUM
+         */
+        if($this->dataSetColumnToSum === null){
+            Throw New \Exception("SET COLUMN TO SUM");
+        }
+        /*
+         * CHECK DATA SET SUM ROW
+         */
+        if($this->dataSetSumRow === null){
+            Throw New \Exception("SET COLUMN ROW TO SUM");
+        }
+        /*
+         * CHECK SUM COLUMN LABEL
+         */
+        if($this->dataSetSumColumnLabel === null || $this->dataSetSumColumnLabelValue === null){
+            Throw New \Exception("SET COLUMN SUM LABEL");
+        }
+    }
+    protected function getTimeInH(int $time=0):float
+    {
+        if($time === 0){
+            return 0;
+        }
+        $halfAnHour = 0;
+        $halfAnHourDiv = 0;
+        $hour = 0;
+        /*
+         * modulo
+         */
+        $modulo = $time % 30;
+        if($modulo>0){
+            $halfAnHour = 0.5;
+            $time = $time - $modulo;
+        }
+        if($time === 0){
+            return $halfAnHour;
+        }
+        /*
+         * GET HOUR
+         */
+        $hour =  $time / 60;
+        if($hour>0){
+            $time = $time - ($hour * 60);
+        }
+        if($time === 0){
+            return $halfAnHour+$hour;
+        }
+        /*
+         * GET HALF AN HOUR
+         */
+        if(($time / 30)>0){
+            return $halfAnHour+$hour+0.5;
+        }
+        return $halfAnHour+$hour;
     }
 }
