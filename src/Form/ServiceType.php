7aba6e07b6d299407c1b3f13226cf9836e0f55dc
Tomasz Borczynski <tborczynski87@gmail.com>
2024-11-02 10:20:50 +0100

    add service type select

--- src/Form/ServiceType.php
+++ src/Form/ServiceType.php
@@ -27,8 +27,8 @@ class ServiceType extends AbstractType
                 'label'=>'Route (in kilometers)'
             ]) 
             ->add('clientPoint', null, [
-                'choice_label' =>  function ($client) {
-                        return $client->getName() . ' (' .$client->getStreet().",". $client->getTown().")";
+                'choice_label' =>  function ($clientPoint) {
+                        return $clientPoint->getName() . ' (' .$clientPoint->getStreet().",". $clientPoint->getTown().")";
                     },
                 'placeholder' => 'Choose a client point',
                 'autocomplete'=> true
@@ -38,6 +38,14 @@ class ServiceType extends AbstractType
                         return $user->getFirstName() ." ".$user->getLastName(). ' (' .$user->getName().")";
                     },
                 'placeholder' => 'Choose a user',
+                'autocomplete'=> true
+            ])
+            ->add('classificationOfActivities', null, [
+                'label'=>'Type of service',
+                'choice_label' =>  function ($classificationOfActivities) {
+                        return "[".$classificationOfActivities->getCode() . '] ' .$classificationOfActivities->getName();
+                    },
+                'placeholder' => 'Choose type of service',
                 'autocomplete'=> true
             ])
         ;
