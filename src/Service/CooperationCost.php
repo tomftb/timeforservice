535057a6b349e566c4c777a31933ab88dba5b0ce
Tomasz Borczynski <tborczynski87@gmail.com>
2024-11-03 11:37:58 +0100

    add new properties

--- src/Service/CooperationCost.php
+++ src/Service/CooperationCost.php
@@ -1,28 +1,53 @@
 <?php
 
-namespace App\Service\Excel;
+namespace App\Service;
+
+use App\Service\CooperationPosition;
+use App\Service\CooperationCostPosition;
 
 /**
- * Description of TimeSum
+ * Description of CooperationCost
  *
  * @author Tomasz Borczynski
  */
-class DistanceSum {
+class CooperationCost {
+    private array $cooperation=[];
     
-    public function __construct(
-        private float $sum = 0
-    )
+    public function __construct()
     {
     }
-    public function add(?float $distance=null):void
+    public function add(?string $code=null,?string $name=null,?string $unit=null, ?float $cost=null, ?float $rate=null, ?float $realTime=null):void
     {
-        if($distance!==null){
-            $this->sum+=$distance;
+        $cooperationPosition = new CooperationPosition();
+        $cooperationPosition->add(
+            $code,
+            $name,
+            $unit,
+            $cost,
+            $rate,
+            $realTime
+        );
+        self::set($cooperationPosition->get());
+    }
+    private function set(CooperationPosition $cooperationPosition):void
+    {
+        $idx = $cooperationPosition->getIdx();
+        if(array_key_exists($cooperationPosition->getIdx(), $this->cooperation)){
+            $this->cooperation[$idx]->setCost($cooperationPosition->getCost());   
+            $this->cooperation[$idx]->setRealTime($cooperationPosition->getRealTime());   
+        }
+        else{
+            $this->cooperation[$idx]=new CooperationCostPosition();
+            $this->cooperation[$idx]->setName($cooperationPosition->getCode()." ".$cooperationPosition->getName());
+            $this->cooperation[$idx]->setUnit($cooperationPosition->getUnit());
+            $this->cooperation[$idx]->setCost($cooperationPosition->getCost());
+            $this->cooperation[$idx]->setRate($cooperationPosition->getRate());
+            $this->cooperation[$idx]->setRealTime($cooperationPosition->getRealTime());
         }
     }
-    
-    public function get():float
+    public function get():array
     {
-        return $this->sum;
+        //dd($this->cooperation);
+        return $this->cooperation;
     }
 }
