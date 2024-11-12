6b60fabc588be02e5e8747e27e268fa3fbceaa17
Tomasz Borczynski <tborczynski87@gmail.com>
2024-11-12 20:44:45 +0100

    new version of delete service

--- src/Form/ServiceDeleteType.php
+++ src/Form/ServiceDeleteType.php
@@ -6,66 +6,14 @@ use App\Entity\Service;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
-use Symfony\Component\Form\Extension\Core\Type\EnumType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use App\Model\TypeOfServiceEnum;
-use App\Model\YesOrNoEnum;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
-class ServiceType extends AbstractType
+class ServiceDeleteType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('description', TextareaType::class,[
-                'label'=>'Description',
-                "attr" => array("rows" => 10)
-            ])
-            ->add('typeOfService',EnumType::class,[
-                'class'=> TypeOfServiceEnum::class,
-                'required' => true,
-                'data_class'=>null,
-            ])
-            ->add('startedAt', DateTimeType::class, [
-                'date_label' => 'Starts On',
-            ]) 
-            ->add('endedAt', DateTimeType::class, [
-                'date_label' => 'Starts On',
-            ])
-            ->add('time',null,[
-                'label'=>'Time (in minutes)'
-            ]) 
-            ->add('route',null,[
-                'label'=>'Route (in kilometers)'
-            ]) 
-            ->add('clientPoint', null, [
-                'choice_label' =>  function ($clientPoint) {
-                        return $clientPoint->getName() . ' (' .$clientPoint->getStreet().",". $clientPoint->getTown().")";
-                    },
-                'placeholder' => 'Choose a client point',
-                'autocomplete'=> true
-            ])
-            ->add('user', null, [
-                'choice_label' => function ($user) {
-                        return $user->getFirstName() ." ".$user->getLastName(). ' (' .$user->getName().")";
-                    },
-                'placeholder' => 'Choose a user',
-                'autocomplete'=> true
-            ])
-            ->add('classificationOfActivities', null, [
-                'label'=>'Type of service',
-                'choice_label' =>  function ($classificationOfActivities) {
-                        return "[".$classificationOfActivities->getCode() . '] ' .$classificationOfActivities->getName();
-                    },
-                'placeholder' => 'Choose type of service',
-                'autocomplete'=> true
-            ])
-            ->add('notified',EnumType::class,[
-                'class'=> YesOrNoEnum::class,
-                'label'=>'Notified',
-                'required' => true,
-                'data_class'=>null,
-            ])
+            ->add('id', HiddenType::class)
         ;
     }
 
