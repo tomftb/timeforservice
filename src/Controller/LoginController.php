0c2ff5c705ac66b5f6ff53d26558e80160878335
Tomasz Borczynski <tborczynski87@gmail.com>
2025-01-25 19:29:24 +0100

    test prototype

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,9 +2,6 @@
 
 namespace App\Controller;
 
-use App\Repository\ClientRepository;
-use App\Repository\ClientPointRepository;
-use App\Repository\ServiceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,37 +11,13 @@ use Symfony\Component\Routing\Annotation\Route;
 use Pagerfanta\Pagerfanta;
 use Pagerfanta\Doctrine\ORM\QueryAdapter;
 
-class MainController extends AbstractController
+class LoginController extends AbstractController
 {
-    #[Route('/', name: 'app_main_homepage')]
-    public function homepage(
-        ServiceRepository $serviceRepository,
-        ClientRepository $clientRepository,
-        ClientPointRepository $clientPointRepository,
-        /* VARIABLE NAME MUST EQUAL URL PART ex. ?page => $page */
-        #[MapQueryParameter] int $page = 1,
-        #[MapQueryParameter] string $sort = 'endedAt',
-        #[MapQueryParameter] string $sortDirection = 'ASC',
-        #[MapQueryParameter] string $query = null,
-        #[MapQueryParameter('clientsPoints', \FILTER_VALIDATE_INT)] array $searchClientsPoints = [],
+    #[Route('/login', name: 'app_login')]
+    public function login(
     ): Response
     {
-        //dd($searchClientsPoints);
-        $validSorts = ['description','clientPoint','user','endedAt'];
-        $sort = in_array($sort,$validSorts) ? $sort : 'endedAt';
-        $maxPerPage = 10;
-        $pager = Pagerfanta::createForCurrentPageWithMaxPerPage(
-                new QueryAdapter($serviceRepository->findBySearchWithClientPointQueryBuilder($query, $searchClientsPoints,$sort,$sortDirection),false),
-                $page,
-                $maxPerPage
-        );
-        return $this->render('main/homepage.html.twig', [
-            'services' => $pager,
-            'clientsPoints'=>$clientPointRepository->findAll(),
-            'clients' => $clientRepository->findAll(),
-            'searchClientsPoints' => $searchClientsPoints,
-            'sort' => $sort,
-            'sortDirection' => $sortDirection
+        return $this->render('login/index.html.twig', [
         ]);
     }
 }
