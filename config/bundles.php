b78c177bfb188aea5d47d9b1e01173e6ee4a2468
Tomasz Borczynski <tborczynski87@gmail.com>
2025-02-01 08:37:55 +0100

    install symfony SecurityBundle and TogglePasswordBundle

--- config/bundles.php
+++ config/bundles.php
@@ -21,4 +21,6 @@ return [
     Symfony\UX\Autocomplete\AutocompleteBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\UX\TogglePassword\TogglePasswordBundle::class => ['all' => true],
 ];
