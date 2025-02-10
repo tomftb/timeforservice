2994a281fe2cb39604d7cf8c2dbe6cd09110769d
Tomasz Borczynski <tborczynski87@gmail.com>
2025-02-10 17:42:49 +0100

    install symfonycasts/verify-email-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -23,4 +23,5 @@ return [
     Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\UX\TogglePassword\TogglePasswordBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
