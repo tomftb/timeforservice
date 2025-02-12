36c69436cffdc67b37a5d2459b616684ddcb6b62
Tomasz Borczynski <tborczynski87@gmail.com>
2025-02-12 16:29:18 +0100

    set default null price

---
+++ migrations/Version20250211212451.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250211212451 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE classification_of_activities ADD price DOUBLE PRECISION DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE classification_of_activities DROP price');
+    }
+}
---
+++ src/Controller/ClientClassificationOfActivitiesController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ClassificationOfActivitiesRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+/**
+ * Description of ClientPointController
+ *
+ * @author Tomasz Borczynski
+ */
+#[Route('/classificationofactivities')]
+class ClassificationOfActivitiesController extends AbstractController{
+    
+    #[Route('/', name: 'app_classificationofactivities_index', methods: ['GET'])]
+    public function index(ClassificationOfActivitiesRepository $classificationOfActivitiesRepository): Response
+    {
+        return $this->render('classificationofactivities/index.html.twig', [
+            'classificationsOfActivities' => $classificationOfActivitiesRepository->findAll(),
+        ]);
+    }
+}
