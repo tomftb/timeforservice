6a5b2622eae545066cf6ed1e2618790ca6fe2029
Tomasz Borczynski <tborczynski87@gmail.com>
2025-04-17 22:06:53 +0200

    install orm fixtures

--- config/bundles.php
+++ config/bundles.php
@@ -24,4 +24,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\UX\TogglePassword\TogglePasswordBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
