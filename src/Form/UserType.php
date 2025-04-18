2f7395cbcc364d29dc7597b210ee7e62cd2a6a0d
Tomasz Borczynski <tborczynski87@gmail.com>
2025-04-18 23:15:35 +0200

    with permissions

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,29 +4,89 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+
+use App\Repository\PermissionRepository;
+use App\Repository\UserPermissionRepository;
 
 class UserType extends AbstractType
 {
+    private PermissionRepository $permissionRepository;
+    private $userPermissions;
+
+    public function __construct(PermissionRepository $permissionRepository)
+    {
+        $this->permissionRepository = $permissionRepository;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $this->userPermissions = $options['data']->getUserPermissions();
+
         $builder
-            ->add('name')
+           ->add('email', EmailType::class, [
+                'label' => 'Adres email*',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please provide your email address.',
+                    ]),
+                    new Email([
+                        'message' => 'Please provide proper email address.',
+                        'mode' => 'html5',
+                    ]),
+                    new Length([
+                        'max' => 180,
+                        'maxMessage' => 'Email address cannot be longer than {{ limit }} characters.',
+                    ]),
+                ],
+                'attr' => [
+                    'placeholder' => 'twój@email.com',
+                    'maxlength' => 180,
+                ],
+            ])
             ->add('firstName')
             ->add('lastName')
-            ->add('password', PasswordType::class, [//PasswordType::class
+            ->add('password', PasswordType::class, [
+                'label' => 'Password*',
                 'always_empty'=>false,
                 'toggle' => true,
-                'empty_data' => ''
+                'empty_data' => $options['data']->getPassword(),
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please provide your uniqe password.',
+                    ]),
+                    new Length([
+                        'max' => 255,
+                        'maxMessage' => 'Password cannot be longer than {{ limit }} characters.',
+                    ]),
+                ],
+                'attr' => [
+                    'placeholder' => '',
+                    'maxlength' => 255,
+                ],
             ])
         ;
+        foreach ($this->permissionRepository->findAll() as $perrmission){
+            $builder->add('permission_'.$perrmission->getId(), CheckboxType::class, [
+                'label' => $perrmission->getName(),
+                'required' => false,
+                'mapped'=>false,
+                'value'=>$perrmission->getId()
+                ,'attr' => [
+                    'checked'=>self::checkUserPermission($perrmission->getId())
+                ],
+            ]);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver): void
@@ -35,4 +95,17 @@ class UserType extends AbstractType
             'data_class' => User::class,
         ]);
     }
+
+    private function checkUserPermission(int $permissionId=0):bool
+    {
+
+        foreach($this->userPermissions as $userPermission){
+            //dd($userPermission->getPermission()->getId());
+            if($userPermission->getPermission()->getId() === $permissionId){
+                return true;
+            }
+         }
+         return false;
+    }
+
 }
