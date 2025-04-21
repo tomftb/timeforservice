cd2eff6ae6da92faaf705b56a41693d642df8814
Tomasz Borczynski <tborczynski87@gmail.com>
2025-04-21 17:02:36 +0200

    add loadUsers with permissions

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,14 +3,25 @@
 namespace App\DataFixtures;
 
 use App\Entity\Permission;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Model\YesOrNoEnum;
 
 class AppFixtures extends Fixture
 {
+    private $passwordHasher;
+    private $permissionsList=[];
+
+    public function __construct( UserPasswordHasherInterface $passwordHasher) {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
         self::loadPermissions($manager);
+        self::loadUsers($manager);
     }
     private function loadPermissions(ObjectManager $manager){
 
@@ -24,39 +35,39 @@ class AppFixtures extends Fixture
                 ,'name'=>'User access'
             ]
             ,[
-                'code'=>'APP_ACCESS'
+                'code'=>'ROLE_APP_ACCESS'
                 ,'name'=>'Application access'
             ]
             ,[
-                'code'=>'MAIN_PAGE_ACCESS'
+                'code'=>'ROLE_MAIN_PAGE_ACCESS'
                 ,'name'=>'Application main page access'
             ]
             ,[
-                'code'=>'SERVICES_ACCESS'
+                'code'=>'ROLE_SERVICES_ACCESS'
                 ,'name'=>'Application services page access'
             ]
             ,[
-                'code'=>'CLIENTS_ACCESS'
+                'code'=>'ROLE_CLIENTS_ACCESS'
                 ,'name'=>'Application clients page access'
             ]
             ,[
-                'code'=>'CLIENTS_POINT_ACCESS'
+                'code'=>'ROLE_CLIENTS_POINT_ACCESS'
                 ,'name'=>'Application clients points page access'
             ]
             ,[
-                'code'=>'INVOICES_ACCESS'
+                'code'=>'ROLE_INVOICES_ACCESS'
                 ,'name'=>'Application invoices page access'
             ]
             ,[
-                'code'=>'USERS_ACCESS'
+                'code'=>'ROLE_USERS_ACCESS'
                 ,'name'=>'Application user page access'
             ]
             ,[
-                'code'=>'EMPLOYEES_ACCESS'
+                'code'=>'ROLE_EMPLOYEES_ACCESS'
                 ,'name'=>'Application employees page access'
             ]
             ,[
-                'code'=>'CLASSIFICATION_OF_ACTIVITIES_ACCESS'
+                'code'=>'ROLE_CLASSIFICATION_OF_ACTIVITIES_ACCESS'
                 ,'name'=>'Application classification of activities page access'
             ]
         ];
@@ -66,6 +77,39 @@ class AppFixtures extends Fixture
             $permission->setCode($data['code']);
             $permission->setName($data['name']);
             $manager->persist($permission);
+            $this->permissionsList[]=$data['code'];
+        }
+
+        $manager->flush();
+    }
+    private function loadUsers(ObjectManager $manager){
+
+        $usersData=[
+            [
+                'email'=>'tborczynski87@gmail.com'
+                ,'password'=>'Haslo1234'
+                ,'first_name'=>'Tomasz'
+                ,'last_name'=>'Borczyński'
+                ,'is_verified'=>1
+                ,'active'=>YesOrNoEnum::YES
+                ,'roles'=>$this->permissionsList
+            ]
+        ];
+
+        foreach ($usersData as $data) {
+            $user = new User();
+            $user->setEmail($data['email']);
+            $hashedPassword = $this->passwordHasher->hashPassword(
+                $user,
+                $data['password']
+            );
+            $user->setPassword($hashedPassword);
+            $user->setFirstName($data['first_name']);
+            $user->setLastName($data['last_name']);
+            $user->setVerified($data['is_verified']);
+            $user->setActive($data['active']);
+            $user->setRoles($data['roles']);
+            $manager->persist($user);
         }
 
         $manager->flush();
